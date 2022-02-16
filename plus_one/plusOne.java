411835dd73fd9aa2e35fa70d0e23ed14c1cea629
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-02-16 13:17:39 +0800

    Create plusOne.java

---
+++ plus_one/plusOne.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+      int[] result;
+      
+      return result;
+    }
+}
+
+public class plusOne {
+  public static void main(String[] args) {
+  }
+}
